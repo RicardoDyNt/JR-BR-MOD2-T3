2037c370dca6bad0c30c668f037d44a7fe6e781d
RicardoDyNt <josersdneto@uis.pe.senac.br>
2022-10-13 18:59:30 -0300

    Tarefa 2:Obstacles(Cactus and Bird)

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -28,15 +28,15 @@ class Dinosaur(Sprite):
         elif self.dino_duck:
             self.duck()    
 
-        if user_input[pygame.K_UP] and not self.dino_jump and not self.dino_duck :
+        if user_input[pygame.K_UP] and not self.dino_jump and not self.dino_duck:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-        elif user_input[pygame.K_DOWN] and not self.dino_jump and not self.dino_duck  :
+        elif user_input[pygame.K_DOWN] and not self.dino_jump and not self.dino_duck:
             self.dino_jump = False
             self.dino_run = False
             self.dino_duck = True
-        elif not self.dino_jump and not self.dino_duck :
+        elif not self.dino_jump and not self.dino_duck:
             self.dino_jump = False
             self.dino_run = True
             self.dino_duck = False
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,7 +1,9 @@
 import pygame
+import random
 
-from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.cactus import CactusSmall, CactusLarge
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import BIRD, SMALL_CACTUS, LARGE_CACTUS
 
 
 class ObstacleManager:
@@ -10,15 +12,20 @@ class ObstacleManager:
 
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))    
-        
+            if random.randint(0,2) == 0:
+                self.obstacles.append((CactusSmall(SMALL_CACTUS)))
+            elif random.randint(0,2) == 1:
+                self.obstacles.append((CactusLarge(LARGE_CACTUS)))
+            elif random.randint(0,2) == 2:
+                self.obstacles.append(Bird(BIRD))
+            
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.dalay(500)
                 game.playing = False
                 break
-
+                   
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)        
